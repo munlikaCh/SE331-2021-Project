7effcc7c4eaabf8c453781e55cd89a03e91fdbf5
munlikaCh <munlika_ch@cmu.ac.th>
2021-08-14 20:46:11 +0700

    try to create Doc forms

---
+++ src/views/DocComm.vue
@@ -0,0 +1,23 @@
+<template>
+  <b>Hometown:</b>  {{ event.hometown }} <br>
+  <!-- <button @click="register"> Register Me </button> -->
+</template>
+
+<script>
+export default {
+  props: ['event'],
+  inject: ['GStore'],
+  methods: {
+    register(){
+      this.$router.push({
+        name: 'EventDetails',
+        params: { id: this.event.id }
+      })
+      this.GStore.flashMessage = 'you are successfully registered for ' + this.event.name
+      setTimeout(() => {
+        this.GStore.flashMessage = ''}
+      ,3000)
+    }
+  }
+}
+</script>
--- src/views/event/Edit.vue
+++ src/views/event/Edit.vue
@@ -7,8 +7,33 @@
 
   <!-- <button @click="edit"> Edit </button> -->
   <br>
-  <button @click="Dcomment"> Doctor's comment </button>
+  <!--<button @click="Dcomment"> Doctor's comment </button> -->
 
+  <div>
+  <form class = "review-form" @submit.prevent ="onSubmit">
+        <h3>Leave a review</h3>
+        <label for="name">Name:</label>
+        <input id="name" v-model="name">
+        <label for="review">Review:</label>
+        <textarea id ="review" v-model="review"></textarea>
+        <label for="rating">Rating:</label>
+        <select id="rating" v-model.number="rating" >
+            <option>5</option>
+            <option>4</option>
+            <option>3</option>
+            <option>2</option>
+            <option>1</option>
+        </select>
+     
+        <label for="isRecommend">Would you recommend this product?:</label>
+        <select id="isRecommend" v-model.number="isRecommend" >
+            <option>recommend</option>
+            <option>not recommend</option>
+        </select>
+
+        <input class="button" type="submit" value="Submit">
+        </form>
+  </div>
 </template>
 <script>
 
@@ -18,7 +43,7 @@ export default {
   methods: {
     Dcomment(){
       this.$router.push({
-        name: 'EventDetails',
+        name: 'DocComm',
         params: { id: this.event.id }
       })
       this.GStore.flashMessage2 = 'The details in ' + this.event.name + ' already edited.'
