0b245a75bb8adf0302a554ea35d3f599df3300ec
munlikaCh <munlika_ch@cmu.ac.th>
2021-08-14 21:08:25 +0700

    add menu Doctor's comment

--- src/views/DocComm.vue
+++ src/views/DocComm.vue
@@ -1,23 +1,100 @@
 <template>
-  <b>Hometown:</b>  {{ event.hometown }} <br>
+ <div id="flashMessage2" v-if="GStore.flashMessage2">
+    {{ GStore.flashMessage2 }}
+  </div>
+  <form class = "review-form" @submit.prevent ="onSubmit">
+        <h3>Doctor's Comment</h3>
+
+        <label for="name">Name:</label>
+        <input id="name" v-model="name">
+        <br>
+        <br>
+        <label for="review">Review:</label>
+        <textarea id ="review" v-model="review"></textarea>
+        <br>
+        <br>
+
+        <input @click="button" class="button" type="submit" value="Submit">
+        </form>
   <!-- <button @click="register"> Register Me </button> -->
+  <!-- =========== list ============== -->
+      <div class="review-container">
+        <h3>Reviews:</h3>
+        <ul>
+            <li v-for="(review, index) in reviews" :key="index">
+            {{review.name}}
+            <br/>
+                "{{review.review}}"
+            <br/>
+            </li>
+        </ul>
+      </div>
+
+    <review-list v-if="reviews.length" :reviews ="reviews"></review-list>
+    <review-form @review-submitted="addReview"></review-form>
+    
 </template>
 
 <script>
 export default {
+  data() {
+        return {
+            name: '',
+            review: '',
+            reviews: []
+        }
+  },
   props: ['event'],
+        reviews: {
+            type: Array,
+            required: true
+        }
+  ,
   inject: ['GStore'],
   methods: {
-    register(){
-      this.$router.push({
-        name: 'EventDetails',
-        params: { id: this.event.id }
-      })
-      this.GStore.flashMessage = 'you are successfully registered for ' + this.event.name
-      setTimeout(() => {
-        this.GStore.flashMessage = ''}
-      ,3000)
-    }
+     onSubmit() {
+            if (this.name === '' || this.review ==='' ) {
+                alert('Review is incomplete . Please fill out every field.')
+                return
+            }
+            let productReview = {
+                name: this.name,
+                review: this.review
+            }
+            this.$emit('review-submitted', productReview)
+            this.name = '',
+                this.review = ''
+        },
+        addReview(review){
+            this.reviews.push(review)
+        }
+    // onSubmit(){
+    //   this.$router.push({
+    //     name: 'EventDetails',
+    //     params: { id: this.event.id }
+    //   })
+    //   this.GStore.flashMessage = 'you are successfully registered for ' + this.event.name
+    //   setTimeout(() => {
+    //     this.GStore.flashMessage = ''}
+    //   ,3000)
+    // }
   }
 }
 </script>
+<style>
+@keyframes redfade{
+  from{
+    background: rgb(84, 196, 80);
+  }
+  to{
+    background: transparent;
+  }
+}
+#flashMessage2{
+  animation-name: redfade;
+  animation-duration: 5s;
+}
+.sizef{
+  font-size: 16px;
+}
+</style>
--- src/views/event/Layout.vue
+++ src/views/event/Layout.vue
@@ -8,6 +8,8 @@
       <router-link :to="{ name: 'EventRegister' }">Hometown</router-link>
       |
       <router-link :to="{ name: 'EventEdit' }">Status</router-link>
+      |
+      <router-link :to="{ name: 'DocComm' }">Doctor's comment</router-link>
       <h1>{{ GStore.event.name }}{{ GStore.event.surname }}</h1>
     <router-view :event="GStore.event" />
     </div>
@@ -16,29 +18,8 @@
 </template>
 <script>
 // import EventService from '@/services/EventService.js'
+
 export default {
-  // props: ['id'],
-  // data() {
-  //   return {
-  //     event: null
-  //   }
-  // },
-  // created() {
-  //   EventService.getEvent(this.id)
-  //     .then((response) => {
-  //       this.event = response.data
-  //     })
-  //     .catch((error) => {
-  //       if (error.response && error.response.status == 404) {
-  //         this.$router.push({
-  //           name: '404Resource',
-  //           params: { resource: 'event' }
-  //         })
-  //       } else {
-  //         this.$router.push({ name: 'NetworkError' })
-  //       }
-  //     })
-  // }
   inject:['GStore']
 }
 </script>
